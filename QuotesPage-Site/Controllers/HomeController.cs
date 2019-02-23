9f79a2a93881bd11b4217c272ee49053a4fa231d
James Brindle <jamie.brindle7@gmail.com>
2019-02-23 19:15:20 +0000

    Migration

--- QuotesPage-Site/Controllers/HomeController.cs
+++ QuotesPage-Site/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace QuotesPage_Site.Controllers
         {
 
 #if DEBUG
-            _connString = "Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\QuotesDB.mdf;Initial Catalog=PoolStatsDB;Integrated Security=True";
+            _connString = "Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=D:\\Sites\\LocalDB\\jb-local-db.mdf;Initial Catalog=PoolStatsDB;Integrated Security=True";
 #else
 
             _connString = privateclass._connString;
